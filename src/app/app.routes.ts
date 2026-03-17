a4377c987012a5a96603587428f0b30c63bb1a91
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-17 11:54:56 +0300

    Add Order page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Order } from './pages/order/order';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+  },
+  {
+    path: 'order',
+    component: Order,
   },
   {
     path: '**',
