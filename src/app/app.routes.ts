4f1edc7fe20f31b136b63360251a290b12461c09
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-17 11:58:36 +0300

    Add Track page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Order } from './pages/order/order';
+import { Track } from './pages/track/track';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'order',
     component: Order,
+  },
+  {
+    path: 'track',
+    component: Track,
   },
   {
     path: '**',
---
+++ src/app/pages/track/track.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track',
+  imports: [],
+  templateUrl: './track.html',
+  styleUrl: './track.css',
+})
+export class Track {
+
+}
