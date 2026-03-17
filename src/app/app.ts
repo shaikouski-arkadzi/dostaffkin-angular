379d4d0c2a2cca1e95041bf6fa10ca25c1e8c92f
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-17 11:50:57 +0300

    Add Home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: '<router-outlet/>САЙТ РАБОТАЕТ',
+  template: '<router-outlet/>',
 })
-export class App {
-}
+export class App {}
